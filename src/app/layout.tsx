722934ca959d20753df971678a02fce83596c358
Ubarrionuevo <ulitito54@hotmail.es>
2025-04-29 21:36:14 -0300

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "UB | Portfolio",
   description: "Desarrollador Full Stack",
+  icons: {
+    icon: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({
@@ -16,9 +19,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <head>
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>💻</text></svg>" />
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
